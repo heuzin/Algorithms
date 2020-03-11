95a67701472a1f2c8f34e9df46fcc60a6cf14e06
heuzin <matheusqs.arq@gmail.com>
2020-03-11 15:59:49 -0400

    stock prices passing test

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -3,7 +3,21 @@
 import sys
 
 def rock_paper_scissors(n):
-  pass 
+  outcomes = []
+  plays = ['rock', "scissor", 'paper']
+
+  def rec_rps(n, result=[]):
+    # print(f"result: {result}")
+    if n == 0:
+      return outcomes.append(result)
+    for play in plays:
+      rec_rps(n-1, result + [play])
+    
+  rec_rps(n, [])
+  return outcomes
+
+# print(rock_paper_scissors(2))
+
 
 
 if __name__ == "__main__":
